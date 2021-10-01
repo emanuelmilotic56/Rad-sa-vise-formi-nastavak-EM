f63b6af3fcce9800a77771791c86062bf0b392b8
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-10-01 18:36:53 +0200

    Add files via upload

---
+++ Rad sa vise formi-nastavak-EM.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Rad_sa_vise_formi_EM
+{
+    partial class frmStudenti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDodaj = new System.Windows.Forms.Button();
+            this.buttonObrisiStudent = new System.Windows.Forms.Button();
+            this.buttonObrisiSve = new System.Windows.Forms.Button();
+            this.listBoxStudenti = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonDodaj
+            // 
+            this.buttonDodaj.Location = new System.Drawing.Point(38, 38);
+            this.buttonDodaj.Name = "buttonDodaj";
+            this.buttonDodaj.Size = new System.Drawing.Size(143, 23);
+            this.buttonDodaj.TabIndex = 0;
+            this.buttonDodaj.Text = "Dodaj novog studenta";
+            this.buttonDodaj.UseVisualStyleBackColor = true;
+            this.buttonDodaj.Click += new System.EventHandler(this.ButtonDodaj_Click);
+            // 
+            // buttonObrisiStudent
+            // 
+            this.buttonObrisiStudent.Location = new System.Drawing.Point(311, 162);
+            this.buttonObrisiStudent.Name = "buttonObrisiStudent";
+            this.buttonObrisiStudent.Size = new System.Drawing.Size(118, 23);
+            this.buttonObrisiStudent.TabIndex = 2;
+            this.buttonObrisiStudent.Text = "Obrisi studenta";
+            this.buttonObrisiStudent.UseVisualStyleBackColor = true;
+            this.buttonObrisiStudent.Click += new System.EventHandler(this.ButtonObrisiStudent_Click);
+            // 
+            // buttonObrisiSve
+            // 
+            this.buttonObrisiSve.Location = new System.Drawing.Point(38, 357);
+            this.buttonObrisiSve.Name = "buttonObrisiSve";
+            this.buttonObrisiSve.Size = new System.Drawing.Size(179, 23);
+            this.buttonObrisiSve.TabIndex = 3;
+            this.buttonObrisiSve.Text = "Obrisi sve studente";
+            this.buttonObrisiSve.UseVisualStyleBackColor = true;
+            this.buttonObrisiSve.Click += new System.EventHandler(this.ButtonObrisiSve_Click);
+            // 
+            // listBoxStudenti
+            // 
+            this.listBoxStudenti.FormattingEnabled = true;
+            this.listBoxStudenti.Location = new System.Drawing.Point(38, 92);
+            this.listBoxStudenti.Name = "listBoxStudenti";
+            this.listBoxStudenti.Size = new System.Drawing.Size(259, 238);
+            this.listBoxStudenti.TabIndex = 4;
+            // 
+            // frmStudenti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxStudenti);
+            this.Controls.Add(this.buttonObrisiSve);
+            this.Controls.Add(this.buttonObrisiStudent);
+            this.Controls.Add(this.buttonDodaj);
+            this.Name = "frmStudenti";
+            this.Text = "frmStudenti";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDodaj;
+        private System.Windows.Forms.Button buttonObrisiStudent;
+        private System.Windows.Forms.Button buttonObrisiSve;
+        private System.Windows.Forms.ListBox listBoxStudenti;
+    }
+}
+
---
+++ Rad sa vise formi-nastavak-EM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rad_sa_vise_formi_EM
+{
+    public partial class frmStudenti : Form
+    {
+        public frmStudenti()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonDodaj_Click(object sender, EventArgs e)
+        {
+            frmUnosNovogStudenta NoviStudent = new frmUnosNovogStudenta();
+            NoviStudent.Show();
+
+        }
+
+        private void ButtonObrisiStudent_Click(object sender, EventArgs e)
+        {
+            if (!(listBoxStudenti.SelectedIndex < 0))
+
+                listBoxStudenti.Items.RemoveAt(listBoxStudenti.SelectedIndex);
+        }
+
+        private void ButtonObrisiSve_Click(object sender, EventArgs e)
+        {
+            listBoxStudenti.Items.Clear();
+        }
+    }
+}
